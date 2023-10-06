f0231a77c10e7986c5a64708ed31cb18f0d89c1e
krunalijain <krunalijain2001@gmail.com>
2023-10-06 04:10:40 +0000

    #34 Invalidate cloudfront stack when content version changes.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
+  }
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
 }
